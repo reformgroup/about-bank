57f688ebab50a4944f1048035d16050f8e7e64a4
Nikolay_Lipovtsev <n.lipovtsev@gmail.com>
2015-03-29 20:41:26 +0300

    Add Users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
-  root  'main_pages#individuals'
-  
-  get 'main_pages/individuals'
-  
-  get 'main_pages/companies'
-  
-  get 'main_pages/banks'
-  
-  get 'main_pages/help'
+  get 'users/new'
 
+  root  'main_pages#individuals'
+  match '/individuals', to: 'main_pages#individuals', via: 'get'
+  match '/companies',   to: 'main_pages#companies',   via: 'get'
+  match '/banks',       to: 'main_pages#banks',       via: 'get'
+  match '/sign_up',     to: 'users#new',              via: 'get'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
